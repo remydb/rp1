42c91335f3d5b8ef028396aeab1d02250afa4521
Remy de Boer <rboer@tallgrass-ZBOXSD-ID12-ID13.(none)>
2013-01-17 16:04:48 +0100

    Added class for neighbour discovery

--- python/daemon.py
+++ python/daemon.py
@@ -9,6 +9,36 @@ from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
+class Find:
+	def get_lladdrs(dev):
+		call(["ping6", "-c4", "-I", dev, "ff02::1"], stdout=PIPE)
+		proc = Popen(["ip", "-6", "neighbor", "show", "dev", dev], stdout=PIPE)
+		output = proc.stdout.read()
+		ndlines = split(output, "\n")
+
+		lladdrs = []
+		for line in ndlines:
+			lladdr = split(line, " ")
+			lladdrs.append(lladdr[0])
+
+		lladdrs.pop()
+		return lladdrs
+
+	def create_hashed_auth(auth_pass, dev):
+		proc = Popen("ifconfig " + dev + " | grep -oE 'fe80::[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*'", stdout=PIPE, shell=True)
+		own_lladdr = proc.stdout.read().rstrip()
+		auth = auth_pass + " " + own_lladdr
+		hashed_auth = sha224(auth).hexdigest() # we should also put a Nonce in here maybe?
+		return hashed_auth
+
+	def check_hashed_auth(hashed_auth, auth_pass, remote_lladdr):
+		check_auth = auth_pass + " " + remote_lladdr
+		hashed_check_auth = sha224(check_auth).hexdigest()
+		if hashed_check_auth == hashed_auth:
+			return 1
+		else:
+			return 0
+
 class RequestHandler(SimpleXMLRPCRequestHandler):
 	rpc_paths = ('/RPC2',)
 
@@ -46,4 +76,5 @@ class Statserv:
 		server.serve_forever()
 
 if __name__ == '__main__':
+	rem_ip = Find()
 	run = Statserv()
