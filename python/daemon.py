b62e678792b0d1343ebc44ccf185d377337cfc30
Remy de Boer <rboer@tallgrass-ZBOXSD-ID12-ID13.(none)>
2013-01-18 16:12:35 +0100

    Working on autoconnect between two daemons Added threaded processing

--- python/daemon.py
+++ python/daemon.py
@@ -3,6 +3,7 @@ import socket
 import sys
 import subprocess
 import time
+import threading
 import socket
 import SocketServer
 from SimpleXMLRPCServer import SimpleXMLRPCServer
@@ -10,9 +11,13 @@ from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 class Find:
-	def get_lladdrs(dev):
-		call(["ping6", "-c4", "-I", dev, "ff02::1"], stdout=PIPE)
-		proc = Popen(["ip", "-6", "neighbor", "show", "dev", dev], stdout=PIPE)
+	def __init__(self):
+		self.auth_pass = "Tallgrass"
+		self.dev = "eth0.11"
+
+	def get_lladdrs(self):
+		subprocess.call(["ping6", "-c4", "-I", self.dev, "ff02::1"], stdout=subprocess.PIPE)
+		proc = subprocess.Popen(["ip", "-6", "neighbor", "show", "dev", dev], stdout=subprocess.PIPE)
 		output = proc.stdout.read()
 		ndlines = split(output, "\n")
 
@@ -24,27 +29,53 @@ class Find:
 		lladdrs.pop()
 		return lladdrs
 
-	def create_hashed_auth(auth_pass, dev):
-		proc = Popen("ifconfig " + dev + " | grep -oE 'fe80::[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*'", stdout=PIPE, shell=True)
+	def create_hashed_auth(self):
+		proc = subprocess.Popen("ifconfig " + self.dev + " | grep -oE 'fe80::[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*'", stdout=PIPE, shell=True)
 		own_lladdr = proc.stdout.read().rstrip()
-		auth = auth_pass + " " + own_lladdr
+		auth = self.auth_pass + " " + own_lladdr
 		hashed_auth = sha224(auth).hexdigest() # we should also put a Nonce in here maybe?
 		return hashed_auth
 
-	def check_hashed_auth(hashed_auth, auth_pass, remote_lladdr):
-		check_auth = auth_pass + " " + remote_lladdr
+	def check_hashed_auth(self, hashed_auth, auth_pass, remote_lladdr):
+		check_auth = self.auth_pass + " " + remote_lladdr
 		hashed_check_auth = sha224(check_auth).hexdigest()
 		if hashed_check_auth == hashed_auth:
 			return 1
 		else:
 			return 0
 
+	def runloop(self):
+		lladdrs = self.get_lladdrs()
+		ownhash = self.create_hashed_auth()
+
+		for lladdr in lladdrs:
+			s = xmlrpclib.ServerProxy('http://[' + lladdr + '%eth0.11]:8000')
+			if s.auth(ownhash) == 1:
+				return lladdr
+
+
 class RequestHandler(SimpleXMLRPCRequestHandler):
-	rpc_paths = ('/RPC2',)
+    def __init__(self, req, addr, server):
+       self.client_ip = addr
+       SimpleXMLRPCRequestHandler.__init__(self, req, addr, server)
+       self.rpc_paths = ('/RPC2',)
+    # def decode_request_content(self, data):
+    #    data = SimpleXMLRPCRequestHandler.decode_request_content(self, data)
+    #    from xml.dom.minidom import parseString
+    #    doc = parseString(data)
+    #    ps = doc.getElementsByTagName('params')[0]
+    #    pdoc = parseString(
+    #         ''' <param><value>
+    #             <string>%s</string>
+    #             </value></param>''' % (self.client_ip,))
+    #    p = pdoc.firstChild.cloneNode(True)
+    #    ps.insertBefore(p, ps.firstChild)
+    #    return doc.toxml()
 
 class Polls:
-	def temp(self):
-		return "TEMP"
+	def auth(self, rcvdhash):
+		print rcvdhash
+		print RequestHandler.client_ip
 
 	def rx(self):
 		cmdGen = cmdgen.CommandGenerator()
@@ -56,17 +87,15 @@ class Polls:
 		)
 		return str(varBinds[0][1])[:-4]
 
-	def tx(self):
-		return "TXVAL"
-
-class Statserv:
+class Statserv(threading.Thread):
 	def __init__(self):
-		p	=	subprocess.Popen(["ip addr show eth0.11 | grep -oE 'fe80::[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*'"], stdout=subprocess.PIPE, shell=True)
-		self.host	=	p.communicate()[0].rstrip() + '%eth0.11'
+		super(Statserv, self).__init__()
+		p	=	subprocess.Popen(["ip addr show eth0 | grep -oE 'fe80::[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*'"], stdout=subprocess.PIPE, shell=True)
+		self.host	=	p.communicate()[0].rstrip() + '%eth0'
 		self.port	=	32323
-		self.listen()
+		#self.listen()
 
-	def listen(self):
+	def run(self):
 		SocketServer.TCPServer.address_family = socket.AF_INET6
 		server = SimpleXMLRPCServer(("::", 8000), requestHandler=RequestHandler)
 		server.register_introspection_functions()
@@ -76,5 +105,13 @@ class Statserv:
 		server.serve_forever()
 
 if __name__ == '__main__':
-	rem_ip = Find()
-	run = Statserv()
+	#findcls = Find()
+	#findcls.
+
+	t = Statserv()
+
+	t.daemon = False
+	t.start()
+	print t.isAlive()
+
+
--- python/poller.py
+++ python/poller.py
@@ -3,16 +3,22 @@
 import socket
 import sys
 import time
+import subprocess
 import xmlrpclib
 
 class Poll:
 	def __init__(self):
-		self.s = xmlrpclib.ServerProxy('http://[fe80::ba27:ebff:fe58:69e2%eth0.11]:8000')
+		self.s = xmlrpclib.ServerProxy('http://[fe80::201:2eff:fe47:26d2%eth0]:8000')
 		#print self.s.system.listMethods()
 
 	# def poll_temp(self):
 	# 	res = self.s.temp()	
 	# 	print res	
+
+	def poll_auth(self):
+		p	=	subprocess.Popen(["ip addr show eth0 | grep -oE 'fe80::[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*'"], stdout=subprocess.PIPE, shell=True)
+		self.host	=	p.communicate()[0].rstrip()
+		res = self.s.auth(self.host)
 
 	def poll_rx(self):
 		res = self.s.rx()
