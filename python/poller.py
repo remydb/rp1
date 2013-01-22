6de7be7a8b4f8e45f438f56c5c90846d7813d37c
Remy de Boer <rboer@tallgrass-ZBOXSD-ID12-ID13.(none)>
2013-01-22 10:38:47 +0100

    Added some math

--- python/poller.py
+++ python/poller.py
@@ -5,6 +5,7 @@ import sys
 import time
 import subprocess
 import xmlrpclib
+from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 class Poll:
 	def __init__(self):
@@ -14,22 +15,23 @@ class Poll:
 		self.s = xmlrpclib.ServerProxy('http://[' + remote_lladdr + '%eth0.11]:8000')
 		#print self.s.system.listMethods()
 
-	# def poll_temp(self):
-	# 	res = self.s.temp()	
-	# 	print res	
-
 	def poll_rx(self):
 		res = self.s.rx()
 		print res
 
-	# def poll_tx(self):
-	# 	res = self.s.tx()
-	# 	print res
+	def snmp_tx(self):
+		cmdGen = cmdgen.CommandGenerator()
+
+		errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
+			cmdgen.CommunityData('public'),
+			cmdgen.UdpTransportTarget(('192.168.168.1', 161)),
+			'1.3.6.1.4.1.12919.6.8.0'
+		)
+		return str(varBinds[0][1])[:-4]
 
 if __name__ == '__main__':
 	run = Poll()
-	func = getattr(run, sys.argv[1])
-	if(callable(func)):
-		func()
-	else:
-		print "Argument is not a valid poll command!"
+	rx = run.poll_rx()
+	tx = run.snmp_tx()
+	diff = tx - rx
+	print diff
